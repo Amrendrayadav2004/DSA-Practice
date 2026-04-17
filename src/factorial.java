c49c21230c4c9bb8f1d22ac4db230bc58bd14d3f
Amrendrayadav2004 <amrendra11a1650@gmail.com>
2026-04-18 02:58:12 +0530

    factorial of number

---
+++ src/factorial.java
@@ -0,0 +1,12 @@
+public class factorial {
+    public static void main(String[] args) {
+        int ans = factorial( 6);
+        System.out.println(ans);
+    }
+    static int factorial(int n){
+        if(n<=1){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+}
