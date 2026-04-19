90b24c00f4bbf930fdf4a03eeeb1bb15bd84fbaa
Amrendrayadav2004 <amrendra11a1650@gmail.com>
2026-04-19 12:00:33 +0530

    find elemnent in thr array

---
+++ src/searchElement.java
@@ -0,0 +1,12 @@
+public class searchElement {
+    public static void main(String[] args) {
+int[] arr = {2,3,1,4,5};
+        System.out.println(find(arr,9,0));
+    }
+    static boolean find(int[] arr,int target,int index){
+        if(index == arr.length){
+            return false;
+        }
+        return arr[index] == target||find (arr,target ,index+1);
+    }
+}
